2b0dfa8fe0210763144c4b985da92bd99d4f429c
danielturek <danielturek@gmail.com>
2016-07-18 20:23:16 -0500

    automated file backup

--- scratch.R
+++ scratch.R
@@ -1,3 +1,25 @@
+library(nimble)
+
+code <- nimbleCode({
+    a ~ dnorm(0, 1)
+})
+constants <- list()
+data <- list()
+inits <- list(a = 0)
+
+Rmodel <- nimbleModel(code, constants, data, inits)
+
+conf <- configureMCMC(Rmodel)
+conf$printSamplers()
+Rmcmc <- buildMCMC(conf)
+
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+set.seed(0)
+Cmcmc$run(10000)
+samples <- as.matrix(Cmcmc$mvSamples)
+apply(samples, 2, mean)
 
 
 
