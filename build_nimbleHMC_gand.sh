3086672ad3e6d6cd639b9d55cd31895b88aba844
danielturek <danielturek@gmail.com>
2021-12-09 18:09:01 -0500

    automated file backup

--- build_nimbleHMC_gand.sh
+++ build_nimbleHMC_gand.sh
@@ -1,10 +1,15 @@
 cd ~/github/nimble/nimbleHMC/
-
 git pull
-
 rm -f nimbleHMC_*.tar.gz
-
 R CMD build nimbleHMC
+
+rm -rf Documents/nimble
+
+## now, in github/nimble/nimble repo,
+## checkout branch ADoak_without_HMC, and run:
+## build ADoak_without_HMC
+
+cp -r ~/temp/builds/ADoak_without_HMC/nimble Documents/
 
 R CMD INSTALL nimbleHMC_*.tar.gz -l ~/Documents/
 
