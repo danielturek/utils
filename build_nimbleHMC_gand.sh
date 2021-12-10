a76cd8d16655e06ef528d05ae88b17e13caaba81
danielturek <danielturek@gmail.com>
2021-12-09 20:43:55 -0500

    updates

--- build_nimbleHMC_gand.sh
+++ build_nimbleHMC_gand.sh
@@ -1,18 +1,19 @@
-cd ~/github/nimble/nimbleHMC/
+
+rm -rf ~/Documents/nimble
+
+cd ~/github/nimble/nimble/packages
+git checkout ADoak_without_HMC
+git pull
+rm -f nimble_*.tar.gz
+R CMD build nimble
+R CMD INSTALL -l ~/Documents nimble
+
+cd ~/github/nimble/nimbleHMC
+git checkout master
 git pull
 rm -f nimbleHMC_*.tar.gz
 R CMD build nimbleHMC
-
-rm -rf Documents/nimble
-
-cd ~/github/nimble/nimble/
-git checkout ADoak_without_HMC
-build ADoak_without_HMC
-
-cp -r ~/temp/builds/ADoak_without_HMC/nimble Documents/
-
-cd ~/github/nimble/nimbleHMC/
-R CMD INSTALL nimbleHMC_*.tar.gz -l ~/Documents/
+R CMD INSTALL -l ~/Documents nimbleHMC
 
 ## use in R scripts:
 ## if(Sys.info()['nodename'] == 'gandalf') library(nimbleHMC, lib.loc = '~/Documents/') else library(nimbleHMC)
