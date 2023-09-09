3616c67f3b927230dccceb1f1619a1a1a2a7ec37
danielturek <danielturek@gmail.com>
2023-09-08 20:26:58 -0400

    updates to build.sh

--- build.sh
+++ build.sh
@@ -6,36 +6,36 @@ rm -f nimble/src/*.o
 rm -f nimble/src/*.so
 rm -f nimble_*.tar.gz
  
-R CMD build \
-  --no-build-vignettes \
-  --no-manual \
-  --no-resave-data \
-  nimble
+R CMD build nimble
+#  --no-build-vignettes \
+#  --no-manual \
+#  --no-resave-data \
+#  nimble
 
 if [ -z "$1" ]
 then
     # argument 1 is the empty string
-    R CMD INSTALL \
-      --no-docs \
-      --no-html \
-      --no-data \
-      --no-demo \
-      --no-multiarch \
-      --no-byte-compile \
-      nimble_*.tar.gz
+    R CMD INSTALL --install-tests nimble_*.tar.gz
+#      --no-docs \
+#      --no-html \
+#      --no-data \
+#      --no-demo \
+#      --no-multiarch \
+#      --no-byte-compile \
+#      nimble_*.tar.gz
 else
     # argument 1 is provided
     rm -rf ~/temp/builds/"$1"
     mkdir ~/temp/builds/"$1"
-    R CMD INSTALL \
-      --no-docs \
-      --no-html \
-      --no-data \
-      --no-demo \
-      --no-multiarch \
-      --no-byte-compile \
-      -l ~/temp/builds/"$1" \
-      nimble_*.tar.gz
+    R CMD INSTALL       -l ~/temp/builds/"$1"       nimble_*.tar.gz
+#      --no-docs \
+#      --no-html \
+#      --no-data \
+#      --no-demo \
+#      --no-multiarch \
+#      --no-byte-compile \
+#      -l ~/temp/builds/"$1" \
+#      nimble_*.tar.gz
     ####VER=$(Rscript -e "cat(gsub('/Resources/.*$', '', gsub('^.*Versions/', '', .libPaths())))")
     ####cp  -r ~/temp/builds/"$1"/nimble /Library/Frameworks/R.framework/Versions/Current/Resources/library
     ####cp  -r ~/temp/builds/"$1"/nimble /Library/Frameworks/R.framework/Versions/"$VER"/Resources/library
