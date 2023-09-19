23cfecc42f3f396a46f1e28820cf8143a5a43426
danielturek <danielturek@gmail.com>
2023-09-19 08:15:30 -0400

    added Preferences.el

---
+++ Preferences.el
@@ -0,0 +1,11 @@
+;;
+;; This file goes in:
+;; ~/Library/Preferences/Aquamacs Emacs/Preferences.el
+;;
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messages* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+
+(load-theme 'tango-dark)
