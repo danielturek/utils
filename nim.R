693b29a553b6fc784c15073b5f3a887dd4a7dff3
Daniel Turek <danielturek@gmail.com>
2024-02-08 05:53:51 -0500

    udated nim.R with timing MCMC samplers

--- nim.R
+++ nim.R
@@ -30,6 +30,12 @@ Cmodel <- compiledList$model; Cmcmc <- compiledList$mcmc
 set.seed(0)
 samples <- runMCMC(Cmcmc, 10000)
 
+Cmcmc$run(10000, time = TRUE)
+times <- Cmcmc$getTimes()
+types <- sapply(conf$getSamplers(), function(x) paste0(x$name, '_', x$target))
+ix <- sort(times, decreasing = TRUE, index.return = TRUE)$ix
+data.frame(time = times[ix], type = types[ix])
+
 colnames(samples)
 samplesSummary(samples)
 library(basicMCMCplots)
