42b3bd711ce9671e86b8adb66bd1f76ac8ce8252
Daniel Turek <danielturek@gmail.com>
2025-05-20 07:11:18 -0400

    updates

--- trashauxfiles.sh
+++ trashauxfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv *-blx.bib    temp/ 2>/dev/null
-mv *.aux        temp/ 2>/dev/null
-mv *.bbl        temp/ 2>/dev/null
-mv *.blg        temp/ 2>/dev/null
-mv *.log        temp/ 2>/dev/null
-mv *.run.xml    temp/ 2>/dev/null
-mv *.synctex.gz temp/ 2>/dev/null
+mv *-blx.bib    ~/.trash/ 2>/dev/null
+mv *.aux        ~/.trash/ 2>/dev/null
+mv *.bbl        ~/.trash/ 2>/dev/null
+mv *.blg        ~/.trash/ 2>/dev/null
+mv *.log        ~/.trash/ 2>/dev/null
+mv *.run.xml    ~/.trash/ 2>/dev/null
+mv *.synctex.gz ~/.trash/ 2>/dev/null
 
 
